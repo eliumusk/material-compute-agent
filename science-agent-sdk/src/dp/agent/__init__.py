13c9dc61b61cd4fb8ea7b308bcc34454b5f219d9
lhappy <lhappy@MacBook-Pro-3.local>
2025-04-28 11:16:11 +0800

    clean

--- science-agent-sdk/src/dp/agent/__init__.py
+++
@@ -1,5 +0,0 @@
-import os
-RUNNING_MODE = os.environ.get("DP_AGENT_RUNNING_MODE") in ["1", "true"]
-if not RUNNING_MODE:
-    from .cli.cli import main
-__all__ = ["main"]
--- science-agent-sdk/src/dp/agent/server/executor/__init__.py
+++
@@ -1,9 +0,0 @@
-from .base_executor import BaseExecutor
-from .dispatcher_executor import DispatcherExecutor
-from .local_executor import LocalExecutor
-
-__all__ = ["BaseExecutor"]
-executor_dict = {
-    "dispatcher": DispatcherExecutor,
-    "local": LocalExecutor,
-}
